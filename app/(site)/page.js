3c834b3ca9694ec2fb3790a65f9f06e711156c25
Saim <s.sad569.ss@gmail.com>
2023-12-19 18:25:59 +0500

    backend integrated

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -35,7 +35,7 @@ async function Home() {
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">
-            Newest songs
+            Latest songs
           </h1>
         </div>
         <PageContent songs={songs} />
